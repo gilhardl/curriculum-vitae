145689951b82bfe50517d47ffd17d22352d18f51
Lucas GILHARD <l.gilhard@gmail.com>
2018-02-06 14:17:50 +0100

    Add bootstrap template
    Issue(home.component.html): L'image du haut ne s'affiche pas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router/src/router';
 
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+    ]),
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: [
+    './home.component.css',
+    './vendor/simple-line-icons/css/simple-line-icons.css',
+    './vendor/bootstrap/css/bootstrap.min.css',
+    './vendor/aircv/css/animate.css',
+    './vendor/aircv/css/layout.min.css',
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
