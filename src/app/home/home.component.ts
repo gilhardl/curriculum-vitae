04b3c26fabae792c482d9516abb95be45fa5e730
Lucas GILHARD <l.gilhard@gmail.com>
2018-02-07 09:03:16 +0100

    Mise en place pour Angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,29 @@ import { Component, OnInit } from '@angular/core';
     './home.component.css',
     './vendor/simple-line-icons/css/simple-line-icons.css',
     './vendor/bootstrap/css/bootstrap.min.css',
-    './vendor/aircv/css/animate.css',
-    './vendor/aircv/css/layout.min.css',
+    './vendor/css/animate.css',
+    './vendor/css/layout.min.css',
   ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  fragment: any;
+
+  constructor(private routerCtrl: Router, private activatedRouteCtrl: ActivatedRoute) { }
 
   ngOnInit() {
+    // Permet de capter les lien anchor
+    this.activatedRouteCtrl.fragment.subscribe( fragment => {
+      this.fragment = fragment;
+    });
+  }
+
+  ngAfterViewChecked() {
+    try {
+        if (this.fragment) {
+            document.querySelector('#' + this.fragment).scrollIntoView();
+        }
+    } catch (e) { }
   }
 
 }
--- src/app/home/vendor/js/layout.js
+++ src/app/home/vendor/js/layout.js
@@ -1,3 +1,4 @@
+
 var Layout = function () {
     
     // detect mobile device
